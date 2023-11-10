3cfebdbaf78d864df2c378c89197bf006c684c49
Nicholas Irvan Winata <93828626+irvanwn@users.noreply.github.com>
2023-11-11 00:01:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.onscroll = function() {
+      if (window.innerWidth > 768) { // Check the window width
+        scrollFunction();
+      }
+    };
+  });
+  
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById("shownav").style.top = "0";
+    } else {
+      document.getElementById("shownav").style.top = "-80px";
+    }
+  }
