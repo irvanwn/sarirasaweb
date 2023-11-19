bc3ba571f2ba2d865c7d97035a2147d12eeae03e
irvanwn <93828626+irvanwn@users.noreply.github.com>
2023-11-19 18:29:00 +0700

    update responsive
    - carousel margin top still different

--- fasilitas.php
+++ fasilitas.php
@@ -47,17 +47,18 @@
         <div class="namalogomobile" ><img src="img/NamaLogo2.png" alt=""></div>
     </nav>
     <div class="container-top">
-        <div class="fasilitas-1">
-            <div class="child-1">
-                <img src="img/IMG20230917114225.jpg" alt="">
-            </div>
-            <div class="child-2">
-                <div class="wrapped">
-                    <h1>Fasilitas Mushola</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in mollitia vero necessitatibus iure! Eum delectus tenetur esse nisi voluptatibus porro commodi repellat, itaque, nemo perspiciatis quis corrupti. Sint, fugiat?</p>
+            <div class="fasilitas-1">
+                <div class="child-1">
+                    <img src="img/IMG20230917114225.jpg" alt="">
+                </div>
+                <div class="child-2">
+                    <div class="wrapped">
+                        <h1>Fasilitas Mushola</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in mollitia vero necessitatibus iure! Eum delectus tenetur esse nisi voluptatibus porro commodi repellat, itaque, nemo perspiciatis quis corrupti. Sint, fugiat?</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        
         <div class="fasilitas-2">
             <div class="child-2">
                 <div class="wrapped">
@@ -69,6 +70,44 @@
                 <img src="img/IMG20230917114225.jpg" alt="">
             </div>
         </div>
+            
+            <div class="fasilitas-1">
+                <div class="child-1">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                <img src="img/IMG20230917114225.jpg" class="d-block w-100" alt="...">
+                            </div>
+                                <div class="carousel-item">
+                                <img src="img/IMG20230917114225.jpg" class="d-block w-100" alt="...">
+                            </div>
+                                <div class="carousel-item">
+                                <img src="img/IMG20230917114225.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                        </div>
+                    </div>
+                <div class="child-2">
+                    <div class="wrapped">
+                        <h1>Fasilitas Mushola</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in mollitia vero necessitatibus iure! Eum delectus tenetur esse nisi voluptatibus porro commodi repellat, itaque, nemo perspiciatis quis corrupti. Sint, fugiat?</p>
+                    </div>
+                </div>
+            </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
