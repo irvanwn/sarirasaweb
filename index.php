d1b10cc83441db948a8e2bef32df350cd13ecc43
irvanwn <93828626+irvanwn@users.noreply.github.com>
2023-11-28 23:50:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -137,7 +137,7 @@ $conn->close();
       <h2>Fasilitas Rumah Makan</h2>
       <div class="hr-center"><div class="line-h2"></div></div>
       <div class="carousell" id="carouselContainer"> 
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3500">
           <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -311,7 +311,7 @@ $conn->close();
           window.location.href = 'fasilitas.php';
         });
 
-        var highlightElements = document.querySelectorAll('.GotoHighlight');
+        var highlightElements = document.querySelectorAll('single-card');
         highlightElements.forEach(function (element) {
           element.addEventListener('click', function() {
             window.location.href = 'highlight.php';
